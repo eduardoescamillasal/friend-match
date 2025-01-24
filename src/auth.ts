6b361532fe2525528205468d93ba7be8b2a1ad7d
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-24 14:10:40 -0600

    prisma models user, account

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,13 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import authConfig from "./auth.config";
-
-const primsa = new PrismaClient();
+import { prisma } from "./lib/prisma";
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  adapter: PrismaAdapter(primsa),
+  adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
 });
