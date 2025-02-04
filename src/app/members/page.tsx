99782355c57ef97786efa70a3f44da5c4bcb3707
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-04 14:34:23 -0600

    getMembers fnc

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import React from "react";
+import { getMembers } from "../actions/memberActions";
 
-export default function MembersPage() {
+export default async function MembersPage() {
+  const members = await getMembers();
+
   return (
     <div>
-      <h3 className="text-3xl">This will be the members page</h3>
-      <Link href="/">Go back home</Link>
+      <ul>
+        {members &&
+          members?.map((member) => <li key={member.id}>{member.name}</li>)}
+      </ul>
     </div>
   );
 }
