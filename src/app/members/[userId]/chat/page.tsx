4eace6f22832b8eaf2c43e4f3fb2418656ff3f55
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-08 11:31:35 -0600

    displaying messages in box

--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -2,19 +2,33 @@ import CardInnerWrapper from "@/components/CardInnerWrapper";
 import React from "react";
 import ChatForm from "./ChatForm";
 import { getMessageThread } from "@/app/actions/messageActions";
+import MessageBox from "./MessageBox";
+import { getAuthUserId } from "@/app/actions/authActions";
 
 export default async function ChatPage({
   params,
 }: {
   params: { userId: string };
 }) {
+  const userId = await getAuthUserId();
   const messages = await getMessageThread(params.userId);
-  console.log({ messages });
-  return (
-    <CardInnerWrapper
-      header="Chat"
-      body={<div>Chat goes here.</div>}
-      footer={<ChatForm />}
-    />
+
+  const body = (
+    <div>
+      {messages.length == 0 ? (
+        "No messages to display"
+      ) : (
+        <div>
+          {messages.map((message) => (
+            <MessageBox
+              key={message.id}
+              message={message}
+              currentUserId={userId}
+            />
+          ))}
+        </div>
+      )}
+    </div>
   );
+  return <CardInnerWrapper header="Chat" body={body} footer={<ChatForm />} />;
 }
