d49daa4431b4c92c18f04fea6c88a93de07f4df8
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-08 20:53:17 -0600

    message box ui

--- src/app/members/[userId]/chat/MessageBox.tsx
+++ src/app/members/[userId]/chat/MessageBox.tsx
@@ -20,6 +20,39 @@ export default function MessageBox({ message, currentUserId }: Props) {
     />
   );
 
+  const messageContentClasses = clsx("flex flex-col w-[50%] px-2 py-1", {
+    "rounded-l-xl rounded-tr-xl text-white bg-blue-100": isCurrentUserSender,
+    "rounded-r-xl rounded-tl-xl border-gray-200 bg-green-100":
+      !isCurrentUserSender,
+  });
+
+  const renderMessageHeader = () => (
+    <div
+      className={clsx("flex items-center w-full", {
+        "justify-between": isCurrentUserSender,
+      })}
+    >
+      {message.dateRead && message.recipientId !== currentUserId ? (
+        <span className="text-xs text-black text-italic">(Read 4 min ago)</span>
+      ) : (
+        <div></div>
+      )}
+      <div className="flex">
+        <span className="text-sm font-semibold text-gray-900">
+          {message.senderName}
+        </span>
+        <span className="text-sm text-gray-500 ml-2">{message.created}</span>
+      </div>
+    </div>
+  );
+
+  const renderMessageContent = () => (
+    <div className={messageContentClasses}>
+      {renderMessageHeader()}
+      <p className="text-sm py-3 text-gray-900">{message.text}</p>
+    </div>
+  );
+
   return (
     <div className="grid grid-rows-1">
       <div
@@ -29,7 +62,7 @@ export default function MessageBox({ message, currentUserId }: Props) {
         })}
       >
         {!isCurrentUserSender && renderAvatar()}
-        <div>Message content</div>
+        {renderMessageContent()}
         {isCurrentUserSender && renderAvatar()}
       </div>
     </div>
