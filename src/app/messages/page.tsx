592fcb22173c03cb29e3caf513b3f08bd648c143
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-13 12:39:31 -0600

    get latest messages api

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import MessageSidebar from "./MessageSidebar";
+import { getMessagesByContainer } from "../actions/messageActions";
 
-export default function MessagesPage() {
+export default async function MessagesPage({
+  searchParams,
+}: {
+  searchParams: { container: string };
+}) {
+  const messages = await getMessagesByContainer(searchParams.container);
+  console.log({ messages });
   return (
     <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
       <div className="col-span-2">
