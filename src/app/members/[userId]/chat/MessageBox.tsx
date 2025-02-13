7ddf76bb42bae241aea7b3cbe01fec80a0e8bef0
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-13 12:17:38 -0600

    keeping the messagebox focused on the latest chat message

--- src/app/members/[userId]/chat/MessageBox.tsx
+++ src/app/members/[userId]/chat/MessageBox.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { MessageDto } from "@/types";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import clsx from "clsx";
 import { Avatar } from "@heroui/react";
 import { transformImageUrl } from "@/lib/util";
@@ -11,6 +13,12 @@ type Props = {
 
 export default function MessageBox({ message, currentUserId }: Props) {
   const isCurrentUserSender = message.senderId === currentUserId;
+  const messageEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageEndRef.current)
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [messageEndRef]);
 
   const renderAvatar = () => (
     <Avatar
@@ -65,6 +73,7 @@ export default function MessageBox({ message, currentUserId }: Props) {
         {renderMessageContent()}
         {isCurrentUserSender && renderAvatar()}
       </div>
+      <div ref={messageEndRef} />
     </div>
   );
 }
