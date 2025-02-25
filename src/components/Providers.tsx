3cfafde8817883fc80cfe5a098c0624bbd661f50
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-25 08:53:12 -0600

    notification channel hook by creating a private channel

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { useNotificationChannel } from "@/hooks/useNotificationChannel";
 import { usePresenceChannel } from "@/hooks/usePresenceChannel";
 import { HeroUIProvider } from "@heroui/react";
 import React, { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default function Providers({
+  children,
+  userId,
+}: {
+  children: ReactNode;
+  userId: string | null;
+}) {
   usePresenceChannel();
+  useNotificationChannel(userId);
   return (
     <HeroUIProvider>
       <ToastContainer
