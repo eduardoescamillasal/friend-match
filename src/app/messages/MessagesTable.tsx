47ccef7c2db8421b3018c1f88e6bd5279b978166
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-02-25 09:33:44 -0600

    refactor logic in messagestable into a hook

--- src/app/messages/MessagesTable.tsx
+++ src/app/messages/MessagesTable.tsx
@@ -2,7 +2,6 @@
 
 import { MessageDto } from "@/types";
 import {
-  Button,
   Card,
   Table,
   TableBody,
@@ -11,68 +10,25 @@ import {
   TableHeader,
   TableRow,
 } from "@heroui/react";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
-import React, { Key, useCallback, useState } from "react";
-import { AiFillDelete } from "react-icons/ai";
-import { deleteMessage } from "../actions/messageActions";
-import { truncateString } from "@/lib/util";
-import PresenceAvatar from "@/components/PresenceAvatar";
+
+import React from "react";
+
 import MessagesTableCell from "./MessagesTableCell";
+import { useMessages } from "@/hooks/useMessages";
 
 type Props = {
   messages: MessageDto[];
 };
 
 export default function MessagesTable({ messages }: Props) {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const isOutbox = searchParams.get("container") === "outbox";
-  const [isDeleting, setIsDeleting] = useState({ id: "", loading: false });
-
-  const columns = [
-    {
-      key: isOutbox ? "recipientName" : "senderName",
-      label: isOutbox ? "Recipient" : "Sender",
-    },
-    {
-      key: "text",
-      label: "Message",
-    },
-    {
-      key: "created",
-      label: isOutbox ? "Date sent" : "Date received",
-    },
-    {
-      key: "actions",
-      label: "Actions",
-    },
-  ];
-
-  const handleDeleteMessage = useCallback(
-    async (message: MessageDto) => {
-      setIsDeleting({ id: message.id, loading: true });
-      await deleteMessage(message.id, isOutbox);
-      router.refresh();
-      setIsDeleting({ id: "", loading: false });
-    },
-    [isOutbox, router]
-  );
-
-  const handleRowSelect = (key: Key) => {
-    const message = messages.find((m) => m.id === key);
-    const url = isOutbox
-      ? `/members/${message?.recipientId}`
-      : `/members/${message?.senderId}`;
-    router.push(url + "/chat");
-  };
-
+  const { columns, isOutbox, isDeleting, deleteMessage, selectRow } =
+    useMessages(messages);
   return (
     <Card className="flex flex-col gap-3 h-[80vh] overflow-auto">
       <Table
         aria-label="Table with messages"
         selectionMode="single"
-        onRowAction={(key) => handleRowSelect(key)}
+        onRowAction={(key) => selectRow(key)}
         shadow="none"
       >
         <TableHeader columns={columns}>
@@ -101,7 +57,7 @@ export default function MessagesTable({ messages }: Props) {
                     item={item}
                     columnKey={columnKey as string}
                     isOutbox={isOutbox}
-                    deleteMessage={handleDeleteMessage}
+                    deleteMessage={deleteMessage}
                     isDeleting={isDeleting.loading && isDeleting.id === item.id}
                   />
                 </TableCell>
